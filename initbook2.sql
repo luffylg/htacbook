0be6e31a9b7765302a1ffa248075d7893a103752
luffylg <lake0105@foxmail.com>
2016-05-18 13:41:01 +0800

    mysql init2

---
+++ initbook2.sql
@@ -0,0 +1,11 @@
+use htacbook;
+create table book (
+    `编号` varchar(50) not null,
+    `书名` varchar(50) not null,
+    `出版社` varchar(50) not null,
+    `作者` varchar(50) not null,
+    `出版时间` varchar(50) not null,
+    `status` varchar(50) not null,
+    `lending_times` varchar(50) not null,
+    primary key (`编号`)
+) engine=innodb default charset=utf8;
